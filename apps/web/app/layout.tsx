5a6c954d8b2d13167690441a447ff4fec6b872b8
test <test@test.com>
2025-09-11 17:51:52 -0700

    Add ClickCease tracking script for click fraud protection

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -29,6 +29,23 @@ export default async function RootLayout({
       <head>
       </head>
       <body>
+        {/* ClickCease.com tracking */}
+        <script
+          nonce={nonce}
+          dangerouslySetInnerHTML={{
+            __html: `var script = document.createElement('script');
+      script.async = true; script.type = 'text/javascript';
+      var target = 'https://www.clickcease.com/monitor/stat.js';
+      script.src = target;var elem = document.head;elem.appendChild(script);`,
+          }}
+        />
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <a href="https://www.clickcease.com" rel="nofollow">
+            <img src="https://monitor.clickcease.com" alt="ClickCease" />
+          </a>
+        </noscript>
+        {/* End ClickCease.com tracking */}
         <RootProviders theme={theme} lang={language} nonce={nonce}>
           {children}
         </RootProviders>
